937e37d3327b294b0c1e2ff88755b29ebf19977e
Fabien <fab@alti-com.fr>
2018-10-12 00:43:16 +0200

    Update home Insta Logo

--- javascript/home.js
+++ javascript/home.js
@@ -10,13 +10,14 @@ $("#hashtag").click(function () {
     }
   });
 
+  
 $("#hashtagReset").click(function () {
     if ($("#inputHashtag").val().length > 2) {
         hashtag = $("#inputHashtag").val();
         idInsta = [];
         map.removeLayer(photoLayer);
         photoLayer = L.photo.cluster({spiderfyDistanceMultiplier: 1.6}).on('click', function (evt) {
-            evt.layer.bindPopup(L.Util.template('<img src="{url}"/><img src="img/logoInsta.png" style="display: inline-block; height: 40px !important; width: 40px !important;"><span><b>Photo de {username}</b></span><span style="float:right;"><img id="imgLike" src="img/instaLike.png"/></span><b style="float: right; line-height:28px;">{likes}</b><p>{caption}</p><br><br>', evt.layer.photo), {
+            evt.layer.bindPopup(L.Util.template('<img src="{url}"/><img src="media/icons/logoInsta.png" style="display: inline-block; height: 40px !important; width: 40px !important;"><span><b>Photo de {username}</b></span><span style="float:right;"><img id="imgLike" src="media/icons/instaLike.png"/></span><b style="float: right; line-height:28px;">{likes}</b><p>{caption}</p><br><br>', evt.layer.photo), {
                 className: 'leaflet-popup-photo',
                 minWidth: 400
             });
@@ -27,7 +28,7 @@ $("#hashtagReset").click(function () {
         idInsta = [];
         map.removeLayer(photoLayer);
         photoLayer = L.photo.cluster({spiderfyDistanceMultiplier: 1.6}).on('click', function (evt) {
-            evt.layer.bindPopup(L.Util.template('<img src="{url}"/><img src="img/logoInsta.png" style="display: inline-block; height: 40px !important; width: 40px !important;"><span><b>Photo de {username}</b></span><span style="float:right;"><img id="imgLike" src="img/instaLike.png"/></span><b style="float: right; line-height:28px;">{likes}</b><p>{caption}</p><br><br>', evt.layer.photo), {
+            evt.layer.bindPopup(L.Util.template('<img src="{url}"/><img src="media/icons/logoInsta.png" style="display: inline-block; height: 40px !important; width: 40px !important;"><span><b>Photo de {username}</b></span><span style="float:right;"><img id="imgLike" src="media/icons/instaLike.png"/></span><b style="float: right; line-height:28px;">{likes}</b><p>{caption}</p><br><br>', evt.layer.photo), {
                 className: 'leaflet-popup-photo',
                 minWidth: 400
             });
